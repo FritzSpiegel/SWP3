1e13a5b7623c20e8d96f1ec8c358fc20a63863a5
FritzSpiegel <fritz.spiegel@student.htldornbirn.at>
2023-10-13 09:17:39 +0200

    String Helper

--- src/at/SPIFR/basics/Stringhelper.java
+++ src/at/SPIFR/basics/Stringhelper.java
@@ -1,1 +1,32 @@
+package at.SPIFR.basics;
 
+public class Stringhelper {
+    String sSatz = "Otto";
+
+    boolean Palindrom = isPalindrom("otto");
+
+
+
+
+    public static boolean isPalindrom (String test){
+
+
+        char[] textarray = test.toCharArray();
+
+        int lange = textarray.length + 1;
+
+        for(int i = 0; i < textarray.length/2; i++) {
+            int hilfe = textarray.length - 1 - i;
+
+            if(textarray[i] == textarray[hilfe]){
+                boolean Ergebnis = true;
+
+            }else {
+
+                boolean Ergebnis = false;
+            }
+        };
+
+        return false;
+    }
+}
