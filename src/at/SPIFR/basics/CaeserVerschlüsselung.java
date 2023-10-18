09fb55b6fb832cf35b5c5d94a5867a27eb3fffe6
FritzSpiegel <fritz.spiegel@student.htldornbirn.at>
2023-10-18 08:38:32 +0200

    String Helper

--- src/at/SPIFR/basics/CaeserVerschlüsselung.java
+++ src/at/SPIFR/basics/CaeserVerschlüsselung.java
@@ -35,8 +35,35 @@ public class CaeserVerschlüsselung {
         String sVerschluesselt = "";
         char[] aString = StringVerschluesseln.toCharArray();
         for (int i = 0; i < aString.length; i++) {
-            int iVerschluesselt = aString[i] + verschiebung;
+            int iVerschluesselt = aString[i];
+
+
+            if(iVerschluesselt == 32){
+
+                iVerschluesselt = 32;
+
+            } else if (iVerschluesselt < 65){
+
+
+
+            }else {
+
+                iVerschluesselt = aString[i] + verschiebung;
+
+            }
+
+            if(iVerschluesselt > 122){
+
+                iVerschluesselt = iVerschluesselt - 26;
+
+            } else if (iVerschluesselt > 90 && iVerschluesselt < 97) {
+
+                iVerschluesselt = iVerschluesselt - 26;
+
+            }
+
             sVerschluesselt = sVerschluesselt + Character.toString(iVerschluesselt);
+
         }
 
 
--- src/at/SPIFR/basics/Stringhelper.java
+++ src/at/SPIFR/basics/Stringhelper.java
@@ -6,46 +6,80 @@ public class Stringhelper {
     public static void main(String[] args) {
 
         String sSatz = "";
+        char  cAuswahl = ' ';
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Geben sie ein Wort ein, von dem sie wissen möchten ob es ein Palindrom ist: ");
+        System.out.println("Geben sie ein Wort ein: ");
 
         sSatz = scanner.nextLine();
 
-        boolean Palindrom = isPalindrom(sSatz);
+        System.out.println("Welche Funktion moechten sie mit ihrem String ausführen?");
+        System.out.println("\tIst es ein Palindrom? (a)");
+        System.out.println("\tWi eviele Buchstaben sind im String? (b)");
 
-        System.out.println(Palindrom);
+        cAuswahl = scanner.next().charAt(0);
+
+
+
+        if(cAuswahl == 'a'){
+            boolean Palindrom = isPalindrom(sSatz);
+
+            System.out.println(Palindrom);
+        }else if(cAuswahl == 'b'){
+
+            int iAnzahl = countLetters(sSatz);
+
+            System.out.println(iAnzahl);
+
+        }
+
 
 
         System.out.println("Möchten sie noch ein weiteres Wort eingeben? \t Ja(j) \t Nein(n)");
 
-
-
-
     }
-    public static boolean isPalindrom (String test){
 
+
+
+
+
+    public static boolean isPalindrom (String test){
 
         boolean Ergebnis = true;
         int ihilfe = 0;
+
+
         char[] textarray = test.toCharArray();
 
 
-        for (int i = 0; i < textarray.length / 2; i++) {
+
+        for (int i = 0; i < textarray.length; i++) {
             int hilfe = textarray.length - 1 - i;
 
             if (textarray[i] == textarray[hilfe]) {
 
             } else {
                 ihilfe = 1;
-
             }
-
         }
-
         if(ihilfe == 1){
             Ergebnis = false;
         }
         return Ergebnis;
+    }
+
+
+    public static int countLetters (String test){
+
+        char[] textarray = test.toCharArray();
+        int iZaehler = 0;
+
+
+        for(int i = 0; i < textarray.length; i ++){
+
+            iZaehler += 1;
+        }
+
+       return iZaehler;
     }
 }
